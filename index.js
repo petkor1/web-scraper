4d347c3a09d1fc551cd57c0b82fce22ba5425fcd
Piotr Korbas <piotrkorbas@gmail.com>
2022-11-16 10:36:18 +0100

    last commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+
+const url = 'https://www.biegigorskie.pl/kalendarz-2022/';
+
+axios(url).then(res=> {
+    const html = res.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+    $('td', html).each(function (){
+        const text = $(this).text();
+        const url = $(this).find('a').attr('href');
+        articles.push({
+            text,
+            url
+        })
+    })
+    console.log(articles)
+})
+
+app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`));
